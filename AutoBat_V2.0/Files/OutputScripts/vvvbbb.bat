e0f45012e642f495a9870b84df61639c9d5f18e5
LiMengyangLMY <1364527938@qq.com>
2026-03-28 08:01:21 +0800

    FirstCommit

--- AutoBat_V2.0/Files/OutputScripts/vvvbbb.bat
+++
@@ -1,8 +0,0 @@
-chcp 65001
-@echo off
-
-start "" "D:\Data\DS\Project\拼豆项目\样例图片\蒜皮宝宝.jpg"
-start "" "D:\Data\DS\Project\拼豆项目\样例图片\线条小狗5.jpg"
-start "" "D:\Data\DS\毕设\文献_bat脚本\No2.基于YOLOv5s的乒乓球捡拾分类机器人设计_邵子宴.pdf"
-start "" "D:\Data\DS\毕设\文献_bat脚本\No3.基于单目视觉的全自动捡球机器人控制算法设计_高长旭.pdf"
-start "" "D:\Data\DS\毕设\文献_bat脚本\No4.一种改进蚁群算法在捡球机器人多目标路径规划中的应用_曹洁.pdf"
